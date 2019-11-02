0936071d39bfa4395a4267e6871fe9d1c5ef6447
michaelspencer <m1373spencer@gmail.com>
2019-11-02 15:40:22 -0700

    updated with preliminary features

--- vw_format.py
+++ vw_format.py
@@ -5,9 +5,71 @@
 ## Notes: Good features may include hour of day, minute of hour, number of capital letters...
 #########################################################################################################
 
+# Libraries
 import sys
-import datetime
+import nltk
+from datetime import datetime as dt
 
-for tweet in sys.stdin:
+# Parameters
+INDEX_WHO = 0
+INDEX_DATETIME = 1
+INDEX_TWEET = 2
+
+
+def get_label(tweeter):
+    if (tweeter == "Trump"):
+        return str("1 ")
+    else: 
+        return str("-1 ")
+
+
+def get_datetime_features(dt_info):
+    dt_object = dt.strptime(dt_info, "%Y-%m-%dT%H:%M:%SZ")
+    return "|time hour:" + str(dt_object.hour) + " minute:" + str(dt_object.minute)
+
+
+def get_text_features(text_info):
     
-    # Parse tweet and format features
+    return ("|text num_caps:" + count_char_type(text_info, "caps") 
+            + " num_ats:" + count_char_type(text_info, "ats") 
+            + " num_hash:" + count_char_type(text_info, "hash")
+            + " " + str("https:" in text_info)
+            + " " + str("\"@" in text_info))
+
+
+def count_char_type(text, char = "caps"):
+    count = 0
+    
+    for letter in text:
+        if (char == "caps" and letter.isupper()):
+            count += 1
+        elif (char == "hash" and letter == "#"):
+            count += 1
+        elif (char == "at" and letter == "@"):
+            count += 1
+    
+    return str(count)
+
+
+for line in sys.stdin:
+    
+    tweet_info = line.split("\t")
+    
+    if (tweet_info[INDEX_WHO] not in ["Trump", "Staff"]):
+        continue
+    
+    label = get_label(tweet_info[INDEX_WHO])
+    time_features = get_datetime_features(tweet_info[INDEX_DATETIME])
+    text_features = get_text_features(tweet_info[INDEX_TWEET])
+    
+    # Print out formatted information, features are below
+    
+    # label 
+    # |time hour:[hour] min_of_hour:[min_of_hour]
+    # |text num_caps:[num_caps] num_ats:[num_ats] num_hash:[num_hash] link retweet text_length:[text_length]
+    print(label, time_features, text_features)
+    print(line)
+    print("")
+    
+    # use nltk line below if we want to parse out the tweet
+    #nltk.tokenize.casual.casual_tokenize(tweet_info[2])
